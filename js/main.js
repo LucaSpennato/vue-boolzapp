57c4c277355a66a0bfa8820f1a3112a2c241408b
LucaSpennato <luca.spennato@gmail.com>
2022-07-11 20:58:56 +0200

    added soft delete in object, using it for soft delete and controls, mouseleave reset the openedMess

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const app = new Vue(
             activeElement: 0,
             newMessage: '',
             search: '',
-            openedMess: -1,
+            openedMess: false,
             isInfoOpen: false,
             // messageToShow: '',
             // messageToShowStatus: '',
@@ -19,17 +19,20 @@ const app = new Vue(
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Hai portato a spasso il cane?',
-                            status: 'sent'
+                            status: 'sent',
+                            isSoftDeleted: false,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Ricordati di stendere i panni',
-                            status: 'sent'
+                            status: 'sent',
+                            isSoftDeleted: false,
                         },
                         {
                             date: '10/01/2020 16:15:22',
                             message: 'Tutto fatto!',
-                            status: 'received'
+                            status: 'received',
+                            isSoftDeleted: false,
                         }
                     ],
                 },
@@ -41,17 +44,20 @@ const app = new Vue(
                         {
                             date: '20/03/2020 16:30:00',
                             message: 'Ciao come stai?',
-                            status: 'sent'
+                            status: 'sent',
+                            isSoftDeleted: false,
                         },
                         {
                             date: '20/03/2020 16:30:55',
                             message: 'Bene grazie! Stasera ci vediamo?',
-                            status: 'received'
+                            status: 'received',
+                            isSoftDeleted: false,
                         },
                         {
                             date: '20/03/2020 16:35:00',
                             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                            status: 'sent'
+                            status: 'sent',
+                            isSoftDeleted: false,
                         }
                     ],
                 },
@@ -63,17 +69,20 @@ const app = new Vue(
                         {
                             date: '28/03/2020 10:10:40',
                             message: 'La Marianna va in campagna',
-                            status: 'received'
+                            status: 'received',
+                            isSoftDeleted: false,
                         },
                         {
                             date: '28/03/2020 10:20:10',
                             message: 'Sicuro di non aver sbagliato chat?',
-                            status: 'sent'
+                            status: 'sent',
+                            isSoftDeleted: false,
                         },
                         {
                             date: '28/03/2020 16:15:22',
                             message: 'Ah scusa!',
-                            status: 'received'
+                            status: 'received',
+                            isSoftDeleted: false,
                         }
                     ],
                 },
@@ -85,12 +94,14 @@ const app = new Vue(
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Lo sai che ha aperto una nuova pizzeria?',
-                            status: 'sent'
+                            status: 'sent',
+                            isSoftDeleted: false,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Si, ma preferirei andare al cinema',
-                            status: 'received'
+                            status: 'received',
+                            isSoftDeleted: false,
                         }
                     ],
                 },
@@ -102,12 +113,14 @@ const app = new Vue(
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Ricordati di chiamare la nonna',
-                            status: 'sent'
+                            status: 'sent',
+                            isSoftDeleted: false,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Va bene, stasera la sento',
-                            status: 'received'
+                            status: 'received',
+                            isSoftDeleted: false,
                         }
                     ],
                 },
@@ -119,17 +132,20 @@ const app = new Vue(
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Ciao Claudia, hai novità?',
-                            status: 'sent'
+                            status: 'sent',
+                            isSoftDeleted: false,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Non ancora',
-                            status: 'received'
+                            status: 'received',
+                            isSoftDeleted: false,
                         },
                         {
                             date: '10/01/2020 15:51:00',
                             message: 'Nessuna nuova, buona nuova',
-                            status: 'sent'
+                            status: 'sent',
+                            isSoftDeleted: false,
                         }
                     ],
                 },
@@ -141,12 +157,14 @@ const app = new Vue(
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Fai gli auguri a Martina che è il suo compleanno!',
-                            status: 'sent'
+                            status: 'sent',
+                            isSoftDeleted: false,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Grazie per avermelo ricordato, le scrivo subito!',
-                            status: 'received'
+                            status: 'received',
+                            isSoftDeleted: false,
                         }
                     ],
                 },
@@ -158,17 +176,20 @@ const app = new Vue(
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Ciao, andiamo a mangiare la pizza stasera?',
-                            status: 'received'
+                            status: 'received',
+                            isSoftDeleted: false,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-                            status: 'sent'
+                            status: 'sent',
+                            isSoftDeleted: false,
                         },
                         {
                             date: '10/01/2020 15:51:00',
                             message: 'OK!!',
-                            status: 'received'
+                            status: 'received',
+                            isSoftDeleted: false,
                         }
                     ],
                 }
@@ -203,7 +224,8 @@ const app = new Vue(
                     this.contacts[this.activeElement].messages.push({
                         date: this.nowTime(),
                         message: this.newMessage,
-                        status: 'sent'
+                        status: 'sent',
+                        isSoftDeleted: false,
                     });
                     this.botReply();
                 }
@@ -269,27 +291,23 @@ const app = new Vue(
                 if (this.openedMess !== index) {
                     this.openedMess = index;
                 } else {
-                    this.openedMess = -1;
+                    this.openedMess = false;
                 }
             },
-
-            // deleteMessage: function (messagetoDelete) {
-            //     messagetoDelete.status = false;
-            //     messagetoDelete.message = '[Il messaggio è stato eliminato]';
-            // },
-
+            mouseleave: function(){
+                this.openedMess = false;
+            },
 
             nowTime: function () {
                 return moment().format("DD/MM/YYYY hh:mm:ss");
             },
 
-            // !
             latestMessage: function (array) {         
                 let messages = array.messages;
                 if(messages.length > 0){
                     return messages[messages.length-1].message;
                 }else{
-                    return messages = 'Nessun messaggio.'
+                    return messages = 'Nessun messaggio.';
                 }       
             },
 
@@ -298,19 +316,29 @@ const app = new Vue(
                 if(messages.length > 0){
                     return messages[messages.length- 1].date;
                 }else{
-                    return messages = ''
+                    return messages = '';
+                }
+            },
+
+            latestSoftDelete: function (array) {
+                let messages = array.messages;
+                if(messages.length > 0){
+                    return messages[messages.length- 1].isSoftDeleted;
+                }else{
+                    return messages = '';
                 }
             },
 
             hardDeleteMessage: function (indexElement) {
                 this.contacts[this.activeElement].messages.splice(indexElement, 1);
+                this.messageInfoIndex = null;
+                this.isInfoOpen = false;
             },
 
             softDeleteMessage: function (messageSoftDelete) {
-                messageSoftDelete.message = '[Il messaggio è stato eliminato]';
+                messageSoftDelete.isSoftDeleted = true; 
             },
 
-            // !
             showInfos(index) {
                 this.isInfoOpen = true;
                 this.messageInfoIndex = index;
